9e86100c1b41e9a108b601fb4e7ed0fd3577ce66
LG Gram <calebjacobs210@gmail.com>
2017-11-11 14:12:48 -0500

    Changed some links

--- he-man.sh
+++ he-man.sh
@@ -1,2 +1,2 @@
 ln -svf /home/cajacobs/.dotfiles/he-man/bspwmrc /home/cajacobs/.config/bspwm/bspwmrc
-ln -svf /home/cajacobs/.dotfiles/he-man/.Xresources /home/cajacobs/.Xresources
+ln -svf /home/cajacobs/.dotfiles/he-man/Xresources /home/cajacobs/.Xresources
